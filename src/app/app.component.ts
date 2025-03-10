71adaf013ed29b3e5ce29d40f2211d6d795ebde4
rambaudAymeric <rambaudaymeric@gmail.com>
2025-03-10 12:04:22 +0100

    nettoyage du composant App (html et css)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { LogoTechnoComponent } from "./logo-techno/logo-techno.component";
 
 @Component({
   selector: 'app-root',
-  imports: [LogoTechnoComponent],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
