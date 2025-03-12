9a6f7139d8c5924b24f3d484aefe75ac89f83035
rambaudAymeric <rambaudaymeric@gmail.com>
2025-03-12 18:19:24 +0100

    le composants texte et la photo de prodil fait

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { LogoTechnoComponent } from "./logo-techno/logo-techno.component";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [LogoTechnoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
