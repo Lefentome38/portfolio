a480dd1ec69017d1e81d6b7c38249e0afe069ddc
rambaudAymeric <rambaudaymeric@gmail.com>
2025-03-19 18:11:36 +0100

    rajout de la listeLogo dans le composants page-carte-projet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { LogoTechnoComponent } from "./composants/logo-techno/logo-techno.component";
 import { BulleCompetencesComponent } from "./composants/bulle-competences/bulle-competences.component";
 import { DevidingBarComponent } from "./composants/deviding-bar/deviding-bar.component";
+import { PageCarteProjetComponent } from "./composants/page-carte-projet/page-carte-projet.component";
 
 @Component({
   selector: 'app-root',
-  imports: [LogoTechnoComponent, BulleCompetencesComponent, DevidingBarComponent],
+  imports: [LogoTechnoComponent, BulleCompetencesComponent, DevidingBarComponent, PageCarteProjetComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
