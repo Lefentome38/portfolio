1963e1f4a347924d63fd4ae4abb6a932d1aea31e
rambaudAymeric <rambaudaymeric@gmail.com>
2025-03-19 14:16:23 +0100

    création du composants et des balises de structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { LogoTechnoComponent } from "./logo-techno/logo-techno.component";
+import { LogoTechnoComponent } from "./composants/logo-techno/logo-techno.component";
+import { BulleCompetencesComponent } from "./composants/bulle-competences/bulle-competences.component";
 
 @Component({
   selector: 'app-root',
-  imports: [LogoTechnoComponent],
+  imports: [LogoTechnoComponent, BulleCompetencesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
