131ee74e2a00197b3a8aacd80d257d051a0a7167
rambaudAymeric <rambaudaymeric@gmail.com>
2025-03-19 14:38:18 +0100

    rajout de l'image sass et boucle for pour la bulle compétenceFrontend

--- src/app/composants/bulle-competences/bulle-competences.component.ts
+++ src/app/composants/bulle-competences/bulle-competences.component.ts
@@ -7,6 +7,13 @@ import { LogoTechnoComponent } from "../logo-techno/logo-techno.component";
   templateUrl: './bulle-competences.component.html',
   styleUrl: './bulle-competences.component.scss'
 })
+
 export class BulleCompetencesComponent {
 
+  listComposantsFrontend = ["react", "ts", "js", "sass", "angular"]
+  listComposantsBackend = ["react", "ts", "js", "sass", "angular"]
+  listComposantsOutils = ["react", "ts", "js", "sass", "angular"]
+
+
 }
+
