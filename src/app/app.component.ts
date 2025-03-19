3ebbc639452f7c034c2b3358cce5cbdd42cad68f
rambaudAymeric <rambaudaymeric@gmail.com>
2025-03-19 17:48:25 +0100

    rajout du composants deviding-bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LogoTechnoComponent } from "./composants/logo-techno/logo-techno.component";
 import { BulleCompetencesComponent } from "./composants/bulle-competences/bulle-competences.component";
+import { DevidingBarComponent } from "./composants/deviding-bar/deviding-bar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [LogoTechnoComponent, BulleCompetencesComponent],
+  imports: [LogoTechnoComponent, BulleCompetencesComponent, DevidingBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
