10c40a444f34849919108f735bde6da246590cee
rambaudAymeric <rambaudaymeric@gmail.com>
2025-03-19 14:16:23 +0100

    création du composants et des balises de structure

---
+++ src/app/composants/bulle-competences/bulle-competences.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { LogoTechnoComponent } from "../logo-techno/logo-techno.component";
+
+@Component({
+  selector: 'app-bulle-competences',
+  imports: [LogoTechnoComponent],
+  templateUrl: './bulle-competences.component.html',
+  styleUrl: './bulle-competences.component.scss'
+})
+export class BulleCompetencesComponent {
+
+}
