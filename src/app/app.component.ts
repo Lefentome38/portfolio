0c028610d4af941fd0130781cd74d17810f76b02
rambaudAymeric <rambaudaymeric@gmail.com>
2025-03-21 11:53:41 +0100

    rajout des composants

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,13 @@ import { LogoTechnoComponent } from "./composants/logo-techno/logo-techno.compon
 import { BulleCompetencesComponent } from "./composants/bulle-competences/bulle-competences.component";
 import { DevidingBarComponent } from "./composants/deviding-bar/deviding-bar.component";
 import { PageCarteProjetComponent } from "./composants/page-carte-projet/page-carte-projet.component";
+import { FullScreenHomeImageComponent } from "./composants/full-screen-home-image/full-screen-home-image.component";
+import { HeaderComponent } from "./composants/header/header.component";
+import { PageProfilComponent } from "./composants/page-profil/page-profil.component";
 
 @Component({
   selector: 'app-root',
-  imports: [LogoTechnoComponent, BulleCompetencesComponent, DevidingBarComponent, PageCarteProjetComponent],
+  imports: [LogoTechnoComponent, BulleCompetencesComponent, DevidingBarComponent, PageCarteProjetComponent, FullScreenHomeImageComponent, HeaderComponent, PageProfilComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
