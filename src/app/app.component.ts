1a48108091476242f951941e5bfa6d575fe5d7f3
RAMBAUD Aymeric <128820017+Lefentome38@users.noreply.github.com>
2025-03-21 11:58:10 +0100

    Revert "logo ok"
    This reverts commit fadd8dc15bbd4b674a93f50c6360921b89fff772.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { LogoTechnoComponent } from "./logo-techno/logo-techno.component";
 
 @Component({
   selector: 'app-root',
-  imports: [LogoTechnoComponent],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-
+  title = 'template';
 }
