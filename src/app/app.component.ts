372b756e58e19963b8f6181b586ade853e63608f
RAMBAUD Aymeric <128820017+Lefentome38@users.noreply.github.com>
2025-03-21 11:58:10 +0100

    Revert "création du composants et des balises de structure"
    This reverts commit 1963e1f4a347924d63fd4ae4abb6a932d1aea31e.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { LogoTechnoComponent } from "./composants/logo-techno/logo-techno.component";
-import { BulleCompetencesComponent } from "./composants/bulle-competences/bulle-competences.component";
+import { LogoTechnoComponent } from "./logo-techno/logo-techno.component";
 
 @Component({
   selector: 'app-root',
-  imports: [LogoTechnoComponent, BulleCompetencesComponent],
+  imports: [LogoTechnoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
