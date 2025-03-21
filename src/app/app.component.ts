28cd2e13531077c203da59669bc51569512abe9a
RAMBAUD Aymeric <128820017+Lefentome38@users.noreply.github.com>
2025-03-21 11:58:10 +0100

    Revert "rajout des composants"
    This reverts commit b2f31834334da5d455d25fe8b33b590a1351673a.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,10 @@ import { LogoTechnoComponent } from "./composants/logo-techno/logo-techno.compon
 import { BulleCompetencesComponent } from "./composants/bulle-competences/bulle-competences.component";
 import { DevidingBarComponent } from "./composants/deviding-bar/deviding-bar.component";
 import { PageCarteProjetComponent } from "./composants/page-carte-projet/page-carte-projet.component";
-import { FullScreenHomeImageComponent } from "./composants/full-screen-home-image/full-screen-home-image.component";
-import { HeaderComponent } from "./composants/header/header.component";
-import { PageProfilComponent } from "./composants/page-profil/page-profil.component";
 
 @Component({
   selector: 'app-root',
-  imports: [LogoTechnoComponent, BulleCompetencesComponent, DevidingBarComponent, PageCarteProjetComponent, FullScreenHomeImageComponent, HeaderComponent, PageProfilComponent],
+  imports: [LogoTechnoComponent, BulleCompetencesComponent, DevidingBarComponent, PageCarteProjetComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
