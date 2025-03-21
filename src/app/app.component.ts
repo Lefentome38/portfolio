3c10ecf4ac3060b8ca1122a34c3d91b5f405dff9
RAMBAUD Aymeric <128820017+Lefentome38@users.noreply.github.com>
2025-03-21 11:58:10 +0100

    Revert "rajout du composants deviding-bar"
    This reverts commit 88759a7f758336030e1a7f3e11a12e7c40086c0b.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { LogoTechnoComponent } from "./composants/logo-techno/logo-techno.component";
 import { BulleCompetencesComponent } from "./composants/bulle-competences/bulle-competences.component";
-import { DevidingBarComponent } from "./composants/deviding-bar/deviding-bar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [LogoTechnoComponent, BulleCompetencesComponent, DevidingBarComponent],
+  imports: [LogoTechnoComponent, BulleCompetencesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
