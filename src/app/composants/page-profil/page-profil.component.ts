b2f31834334da5d455d25fe8b33b590a1351673a
rambaudAymeric <rambaudaymeric@gmail.com>
2025-03-21 11:53:41 +0100

    rajout des composants

---
+++ src/app/composants/page-profil/page-profil.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { LogoTechnoComponent } from "../logo-techno/logo-techno.component";
+
+@Component({
+  selector: 'app-page-profil',
+  imports: [LogoTechnoComponent],
+  templateUrl: './page-profil.component.html',
+  styleUrl: './page-profil.component.scss'
+})
+export class PageProfilComponent {
+
+}
