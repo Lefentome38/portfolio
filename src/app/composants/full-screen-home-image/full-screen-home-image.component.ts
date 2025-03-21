dc7f4f47284caba0eba03d76c473e4183ebb2974
rambaudAymeric <rambaudaymeric@gmail.com>
2025-03-21 11:53:41 +0100

    rajout des composants

---
+++ src/app/composants/full-screen-home-image/full-screen-home-image.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-full-screen-home-image',
+  imports: [],
+  templateUrl: './full-screen-home-image.component.html',
+  styleUrl: './full-screen-home-image.component.scss'
+})
+export class FullScreenHomeImageComponent {
+
+}
