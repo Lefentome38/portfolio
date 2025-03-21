daa6ae49f16f06c14203c2e9dd03313c2be63a11
RAMBAUD Aymeric <128820017+Lefentome38@users.noreply.github.com>
2025-03-21 11:58:10 +0100

    Revert "rajout de la listeLogo dans le composants page-carte-projet"
    This reverts commit 789c19095d6c3f7537d51a3f4cc188fddf504461.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { LogoTechnoComponent } from "./composants/logo-techno/logo-techno.component";
 import { BulleCompetencesComponent } from "./composants/bulle-competences/bulle-competences.component";
 import { DevidingBarComponent } from "./composants/deviding-bar/deviding-bar.component";
-import { PageCarteProjetComponent } from "./composants/page-carte-projet/page-carte-projet.component";
 
 @Component({
   selector: 'app-root',
-  imports: [LogoTechnoComponent, BulleCompetencesComponent, DevidingBarComponent, PageCarteProjetComponent],
+  imports: [LogoTechnoComponent, BulleCompetencesComponent, DevidingBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
