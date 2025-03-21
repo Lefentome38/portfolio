87706976667116538c3f8c4b1e37855b4bdbe830
RAMBAUD Aymeric <128820017+Lefentome38@users.noreply.github.com>
2025-03-21 11:58:10 +0100

    Revert "ajout de la grid"
    This reverts commit 49beb413001c88fa7eed0bd1b4acca9b973de29c.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,10 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './menu/landing-page/landing-page.component';
+import { Page1Component } from './body/page1/page1.component';
+import { Page2Component } from './body/page2/page2.component';
 
 export const routes: Routes = [
-  // {path: '', component: LandingPageComponent},
+  {path: 'page1', component: Page1Component},
+  {path: 'page2', component: Page2Component},
+  {path: '', component: LandingPageComponent},
 ];
