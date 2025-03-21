f6b5cd4be0722fd945a5e1a25965e787759e86c9
RAMBAUD Aymeric <128820017+Lefentome38@users.noreply.github.com>
2025-03-21 11:58:10 +0100

    Revert "ajout de la grid"
    This reverts commit 49beb413001c88fa7eed0bd1b4acca9b973de29c.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "./menu/header/header.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [HeaderComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
