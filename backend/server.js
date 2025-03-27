02af2d33689a587a40b2d09fd3dd79df2fd60be6
rambaudAymeric <rambaudaymeric@gmail.com>
2025-03-27 17:46:05 +0100

    bon sa marche a moitier '-'

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+
 const express = require("express");
 const nodemailer = require('nodemailer');
 const cors = require('cors');
@@ -10,26 +11,37 @@ app.use(cors());
 const transport = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   }
 });
 
+const sendMail = async ({name, mail, message}) => {
+  return await transport.sendMail({
+    from: process.env.MAIL_USER,
+    to: process.env.MAIL_USER,
+    subject: "messagePortfolio",
+    html: `
+      <p><strong>Nom:</strong> ${name}</p>
+      <p><strong>Email:</strong> ${mail}</p>
+      <p><strong>Message:</strong> ${message}</p>
+    `
+  })
+}
+
+console.log("MAIL_USER " + process.env.MAIL_USER);
+
 app.post('/send-mail', (req, res) => {
   const {name, email, message} = req.body;
-  const mailOption = {
-    from: email,
-    to: process.env.EMAIL_USER,
-    subject: `Nouveau message de ${name}`,
-    text: message
-  };
 
-  transport.sendMail(mailOption, (error, info) => {
-    if (error) {
-      return res.status(500).send(error.toString());
-    }
-    res.send('Email envoyé : ' + info.response)
-  })
+  
+
+  // transport.sendMail((error, info) => {
+  //   if (error) {
+  //     return res.status(500).send(error.toString());
+  //   }
+      res.status(200).send('mail envoyé' + sendMail(name, email, message))
+  // })
 })
 
 app.listen(3000, () => console.log('Serveur backend lancé sur http://localhost:3000'));
